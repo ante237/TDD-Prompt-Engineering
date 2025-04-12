d457aba3169c679225362cc963e575808687d433
ant237 <anhb20@student.bth.se>
2025-04-12 23:22:29 +0200

    Added manual tests and implementation

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ This module provides functionality to build an itinerary of upcoming concerts.
 """
 
 import math
+from datetime import datetime
 
 class Concert:
     """
@@ -25,14 +26,53 @@ class Concert:
         self.latitude = latitude
         self.longitude = longitude
 
+    def print_class(self):
+        print(f"[\nartist: {self.artist}\ndate: {self.date}\nlocation: {self.location}\nlat: {self.latitude}\nlong: {self.longitude}")
+
 class ItineraryBuilder:
     """
     A class to build concert itineraries. 
     """
     
-    def build_itinerary(self, concerts):
-        return []
+    def quicksort(self, lst):
 
+        less = []
+        eq = []
+        greater = []
+
+        if(len(lst) > 1):
+            pivot = datetime.fromisoformat(lst[0].date)
+            for x in lst:
+                if(datetime.fromisoformat(x.date) < pivot):
+                    less.append(x)
+                elif(datetime.fromisoformat(x.date) == pivot):
+                    eq.append(x)
+                else:
+                    greater.append(x)
+            return self.quicksort(less)+eq+self.quicksort(greater)
+        else:
+            return lst
+
+    def build_itinerary(self, concerts):
+        itinerary = []
+
+        for c in concerts:
+            index = -1
+            for i in range(len(itinerary)):
+                if(c.artist == itinerary[i].artist):
+                    index = i
+                    break
+            if(index == -1):
+                itinerary.append(c)
+            else:
+                currentDate = datetime.fromisoformat(c.date)
+                newDate = datetime.fromisoformat(itinerary[index].date)
+                if(newDate > currentDate):
+                    itinerary[index] = c
+
+
+        itinerary = self.quicksort(itinerary)
+        return itinerary
 
 if __name__ == "__main__":
     from concerts_data import get_all_concerts
--- test.py
+++ test.py
@@ -6,6 +6,7 @@ Participants will implement tests based on the system specifications.
 """
 
 import unittest
+from datetime import datetime
 from main import Concert, ItineraryBuilder
 from concerts_data import get_all_concerts
 
@@ -22,10 +23,73 @@ class ItineraryBuilderTest(unittest.TestCase):
     # Participants will implement their manual test cases here. 
     
     def test_manual_1(self):
-        """First manually written test case."""
-        # TODO: Implement this test
-        pass
-    
+        """Some artists may have no concerts on the list. In that case, that should be indicated in the itinerary."""
+        itinerary = self.builder.build_itinerary(self.all_concerts)
+        allArtists = []
+        listedArtists = []
+
+        for i in range(len(self.all_concerts)):
+            if(self.all_concerts[i].artist not in allArtists):
+                allArtists.append(self.all_concerts[i].artist)
+        
+        for j in range(len(itinerary)):
+            if(itinerary[j].artist not in listedArtists):
+                listedArtists.append(itinerary[j].artist)
+
+        sortedAllArtists = sorted(allArtists)
+        sortedListedArtists = sorted(listedArtists)
+
+        self.assertTrue(itinerary)
+        self.assertEqual(sortedAllArtists, sortedListedArtists)
+        
+
+    def test_manual_2(self):
+        """The itinerary should return a list of concerts sorted in chronological order (by date from earliest to latest)"""
+        itinerary = self.builder.build_itinerary(self.all_concerts)
+        for it in itinerary:
+            it.print_class()
+        dateList = []
+
+        for i in range(len(itinerary)):
+            dateList.append(datetime.fromisoformat(itinerary[i].date))
+
+        self.assertTrue(itinerary)
+        self.assertTrue(dateList == sorted(dateList))
+
+    def test_manual_3(self):
+        """An artist has at most one concert in the itinerary. If an artist has more than one concert in the list, the itinerary should only include the one with the earliest start date."""
+        itinerary = self.builder.build_itinerary(self.all_concerts)
+        foundArtists = []
+        foundDupe = False
+        onlyFirstDate = True
+        concertDates = {}
+
+        for c in self.all_concerts:
+            if(c.artist not in concertDates):
+                concertDates[c.artist] = [datetime.fromisoformat(c.date)]
+            else:
+                tmp = concertDates[c.artist]
+                tmp.append(datetime.fromisoformat(c.date))
+                concertDates[c.artist] = tmp
+
+        for key in concertDates:
+            concertDates[key] = sorted(concertDates[key])
+
+        for i in range(len(itinerary)):
+            if(itinerary[i].artist in foundArtists):
+                foundDupe = True
+                break
+            date = datetime.fromisoformat(itinerary[i].date)
+            if(date != concertDates[itinerary[i].artist][0]):
+                onlyFirstDate = False
+                break
+            foundArtists.append(itinerary[i])
+
+        self.assertTrue(itinerary)
+        self.assertTrue(onlyFirstDate)
+        self.assertFalse(foundDupe)
+
+
     # ----- AI-Assisted Test Cases -----
     # Participants will implement their AI-assisted test cases here.
     # Please name your test in a way which indicates that these are AI-assisted test cases.
